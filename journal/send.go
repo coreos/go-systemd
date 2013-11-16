293430ef0a18d57c4c39ec97b8c0f48d8c4fd8ec
Xiang Li <xiang.li@coreos.com>
2013-11-15 21:52:08 -0800

    refactor(send.go) use fprintln to make consistent

--- journal/send.go
+++ journal/send.go
@@ -103,7 +103,7 @@ func appendVariable(w io.Writer, name, value string) {
 		fmt.Fprintln(w, value)
 	} else {
 		/* just write the variable and value all on one line */
-		fmt.Fprintf(w, "%s=%s\n", name, value)
+		fmt.Fprintln(w, "%s=%s", name, value)
 	}
 }
 
