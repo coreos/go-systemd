a7e90d66aa4d3eee87899c629e97a778de447eba
Brandon Philips <brandon@ifup.co>
2014-01-24 10:32:43 -0800

    fix(*): add space between license and package header
    Fixes this mistake from ae95250ce5abe29f1af2a737caabbfdb561918c0 which
    causes godoc to output a lot of noise.

--- dbus/dbus.go
+++ dbus/dbus.go
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 // Integration with the systemd D-Bus API.  See http://www.freedesktop.org/wiki/Software/systemd/dbus/
 package dbus
 
