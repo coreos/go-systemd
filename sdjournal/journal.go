coreos/go-systemd

commit 7f0723f2757beb369312e795c56cb681a928d7c7

util/sdjournal: minor style cleanup

sdjournal/journal.go
@@ -88,8 +88,8 @@ func NewJournal() (*Journal, error) {
 }
 
 // NewJournalFromDir returns a new Journal instance pointing to a journal residing
-// in a given directory.  Typically, the directory will be appended with /<machine-id>
-// which can be generated from util.GetMachineID() from this same library.
+// in a given directory. The supplied path may be relative or absolute; if
+// relative, it will be converted to an absolute path before being opened.
 func NewJournalFromDir(path string) (*Journal, error) {
 	path, err := filepath.Abs(path)
 	if err != nil {
@@ -100,9 +100,9 @@ func NewJournalFromDir(path string) (*Journal, error) {
 	defer C.free(unsafe.Pointer(p))
 
 	j := &Journal{}
-	errInt := C.sd_journal_open_directory(&j.cjournal, p, 0)
-	if errInt < 0 {
-		return nil, fmt.Errorf("failed to open journal in directory %v: %v", path, errInt)
+	err := C.sd_journal_open_directory(&j.cjournal, p, 0)
+	if err < 0 {
+		return nil, fmt.Errorf("failed to open journal in directory %v: %v", path, err)
 	}
 
 	return j, nil

util/util.go
@@ -246,7 +246,7 @@ func CurrentUnitName() (unit string, err error) {
 }
 
 // IsRunningSystemd checks whether the host was booted with systemd as its init
-// system. This functions similar to systemd's `sd_booted(3)`: internally, it
+// system. This functions similarly to systemd's `sd_booted(3)`: internally, it
 // checks whether /run/systemd/system/ exists and is a directory.
 // http://www.freedesktop.org/software/systemd/man/sd_booted.html
 func IsRunningSystemd() bool {
@@ -257,10 +257,10 @@ func IsRunningSystemd() bool {
 	return fi.IsDir()
 }
 
-// GetMachineID returns a host's 128-bit machine ID as a string. There is a systemd
-// library function, sd_id128_get_machine, which can be used to obtain the
-// machine ID.  However, this function simply reads the string from /etc/machine-id.
-// To simplify things, we'll just read it directly from the file, too.
+// GetMachineID returns a host's 128-bit machine ID as a string. This functions
+// similarly to systemd's `sd_id128_get_machine`: internally, it simply reads
+// the contents of /etc/machine-id
+// http://www.freedesktop.org/software/systemd/man/sd_id128_get_machine.html
 func GetMachineID() (string, error) {
 	machineID, err := ioutil.ReadFile("/etc/machine-id")
 	if err != nil {

commit 97e8788e5fec4292c722714be31bbaa220a41df1

sdjournal: un-break errint->err replacement
7f0723f2757beb369312e795c56cb681a928d7c7 inadvertently broke the build
by re-using a variable; this fixes that.

sdjournal/journal.go
@@ -100,9 +100,9 @@ func NewJournalFromDir(path string) (*Journal, error) {
 	defer C.free(unsafe.Pointer(p))
 
 	j := &Journal{}
-	err := C.sd_journal_open_directory(&j.cjournal, p, 0)
-	if err < 0 {
-		return nil, fmt.Errorf("failed to open journal in directory %v: %v", path, err)
+	errInt := C.sd_journal_open_directory(&j.cjournal, p, 0)
+	if errInt < 0 {
+		return nil, fmt.Errorf("failed to open journal in directory %v: %v", path, errInt)
 	}
 
 	return j, nil