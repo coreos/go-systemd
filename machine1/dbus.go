b9c48833369f3906c0ed5b302f55bc61aa7f682b
Joe Julian <jjulian@io.com>
2016-11-08 11:00:59 -0800

    Add more machine1 support

--- machine1/dbus.go
+++ machine1/dbus.go
@@ -18,15 +18,19 @@ limitations under the License.
 package machine1
 
 import (
+	"fmt"
 	"os"
 	"strconv"
 
 	"github.com/godbus/dbus"
+	"syscall"
 )
 
 const (
 	dbusInterface = "org.freedesktop.machine1.Manager"
 	dbusPath      = "/org/freedesktop/machine1"
+	SIGRTMIN      = syscall.Signal(32)
+	SIGINT        = syscall.SIGINT
 )
 
 // Conn is a connection to systemds dbus endpoint.
@@ -73,6 +77,91 @@ func (c *Conn) initConnection() error {
 	c.object = c.conn.Object("org.freedesktop.machine1", dbus.ObjectPath(dbusPath))
 
 	return nil
+}
+
+// Close closes the system bus connection
+func (c *Conn) Close() error {
+	return c.conn.Close()
+}
+
+// GetImage may be used to get the image object path for the image with the specified name
+func (c *Conn) GetImage(name string) (dbus.ObjectPath, error) {
+	var path dbus.ObjectPath
+	result := c.object.Call(dbusInterface+".GetImage", 0, name)
+	if result.Err != nil {
+		return path, result.Err
+	}
+	pathstr, ok := result.Body[0].(dbus.ObjectPath)
+	if ok {
+		path = pathstr
+	} else {
+		return path, fmt.Errorf("Error converting '%v' to dbus.ObjectPath.", result.Body[0])
+	}
+	return path, nil
+}
+
+// GetMachine retrieves the machine object path from dbus
+func (c *Conn) GetMachine(name string) (dbus.ObjectPath, error) {
+	var path dbus.ObjectPath
+	result := c.object.Call(dbusInterface+".GetMachine", 0, name)
+	if result.Err != nil {
+		return path, result.Err
+	}
+	pathstr, ok := result.Body[0].(dbus.ObjectPath)
+	if ok {
+		path = pathstr
+	} else {
+		return path, fmt.Errorf("Error converting '%v' to dbus.ObjectPath.", result.Body[0])
+	}
+	return path, nil
+}
+
+// GetProperties retrieves the MachineProperties for any given machine
+func (c *Conn) GetProperties(name string) (map[string]interface{}, error) {
+	var machine map[string]interface{}
+	var path dbus.ObjectPath
+	var props map[string]dbus.Variant
+	result := c.object.Call(dbusInterface+".GetMachine", 0, name)
+	if result.Err != nil {
+		return machine, result.Err
+	}
+	pathstr, ok := result.Body[0].(dbus.ObjectPath)
+	if ok {
+		path = pathstr
+	} else {
+		return machine, fmt.Errorf("Error converting '%v' to dbus.ObjectPath.", result.Body[0])
+	}
+	obj := c.conn.Object("org.freedesktop.machine1", path)
+	err := obj.Call("org.freedesktop.DBus.Properties.GetAll", 0, "").Store(&props)
+	if err != nil {
+		return machine, err
+	}
+
+	machine = make(map[string]interface{}, len(props))
+	for k, v := range props {
+		machine[k] = v.Value()
+	}
+	return machine, nil
+}
+
+// KillMachine signals nspawn
+func (c *Conn) KillMachine(name string, killsignal syscall.Signal) error {
+	return c.object.Call(dbusInterface+".KillMachine", 0, name, "leader", killsignal).Err
+}
+
+// PoweroffMachine signals systemd to start the poweroff target
+func (c *Conn) PoweroffMachine(name string) error {
+	return c.KillMachine(name, SIGRTMIN+4)
+}
+
+// RebootMachine signals nspawn to reboot. Works with systemd and sysvinit.
+func (c *Conn) RebootMachine(name string) error {
+	return c.KillMachine(name, SIGINT)
+}
+
+// TerminateMachine terminates the nspawn container
+func (c *Conn) TerminateMachine(name string) error {
+	return c.object.Call(dbusInterface+".TerminateMachine", 0, name).Err
 }
 
 // RegisterMachine registers the container with the systemd-machined
