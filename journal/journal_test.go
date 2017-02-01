2847fc8a8011be00a502f7e6ba9f875f858e2d6a
Luca Bruno <luca.bruno@coreos.com>
2017-02-01 17:31:42 +0000

    journal: add some send tests
    This commit introduces tests exercising sending logic for
    empty/small/large messages.

--- journal/journal_test.go
+++ journal/journal_test.go
@@ -15,6 +15,10 @@
 package journal
 
 import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
 	"testing"
 )
 
@@ -37,6 +41,61 @@ func TestValidaVarName(t *testing.T) {
 		valid := validVarName(tt.testcase)
 		if valid != tt.valid {
 			t.Fatalf("expected %t, got %t", tt.valid, valid)
+		}
+	}
+}
+
+func TestJournalSend(t *testing.T) {
+	// an always-too-big value (hopefully)
+	hugeValue := 1234567890
+
+	// a value slightly larger than default limit,
+	// see `SO_SNDBUF` in socket(7)
+	largeValue := hugeValue
+	if wmem, err := ioutil.ReadFile("/proc/sys/net/core/wmem_default"); err == nil {
+		wmemStr := strings.TrimSpace(string(wmem))
+		if v, err := strconv.Atoi(wmemStr); err == nil {
+			largeValue = v + 1
+		}
+	}
+	// See https://github.com/coreos/go-systemd/pull/221#issuecomment-276727718
+	_ = largeValue
+
+	// small messages should go over normal data,
+	// larger ones over temporary file with fd in ancillary data
+	testValues := []struct {
+		label string
+		len   int
+	}{
+		{
+			"empty message",
+			0,
+		},
+		{
+			"small message",
+			5,
+		},
+		/* See https://github.com/coreos/go-systemd/pull/221#issuecomment-276727718
+		{
+			"large message",
+			largeValue,
+		},
+		{
+			"huge message",
+			hugeValue,
+		},
+		*/
+	}
+
+	for i, tt := range testValues {
+		t.Logf("journal send test #%v - %s (len=%d)", i, tt.label, tt.len)
+		largeVars := map[string]string{
+			"KEY": string(make([]byte, tt.len)),
+		}
+
+		err := Send(fmt.Sprintf("go-systemd test #%v - %s", i, tt.label), PriCrit, largeVars)
+		if err != nil {
+			t.Fatalf("#%v: failed sending %s: %s", i, tt.label, err)
 		}
 	}
 }
