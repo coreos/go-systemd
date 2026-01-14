c3f118dbeb0f63b4fc91ad9742042a763c34f04e
Aaron Paterson <apaterson@protonmail.com>
2026-01-14 09:31:15 -0700

    activation: update files.go copyright

--- activation/files.go
+++ activation/files.go
@@ -1,4 +1,5 @@
-// Copyright 2015 CoreOS, Inc.
+// Copyright 2025 CoreOS, Inc.
+// Copyright 2025 RedHat, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
